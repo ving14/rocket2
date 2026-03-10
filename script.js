dc075c8613f0b21628492197a46661eabc9f2657
ving14 <ving14@stanford.edu>
2026-03-10 14:04:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
+const btn = document.querySelector('a');
+const stateName = document.querySelector('p');
 
-        const btn = document.querySelector('a')
-        const stateName = document.querySelector('p')
+const r = new rive.Rive({
+  src: 'bird.riv', // your file in GitHub repo
+  canvas: document.getElementById('canvas'),
+  autoplay: true,
+  stateMachines: 'State Machine 1',
+  fit: rive.Fit.cover,
+  onLoad: () => {
+    const inputs = r.stateMachineInputs('State Machine 1');
+    const bumpTrigger = inputs.find(i => i.name === 'Trigger 2');
 
-        const r = new rive.Rive({
-            src: 'https://assets.codepen.io/2621168/new_file_1.riv',
-            canvas: document.getElementById('canvas'),
-            autoplay: true,
-            stateMachines: 'State Machine 1',
-            fit: rive.Fit.cover,
-            onLoad: (_) => {
-                const inputs = r.stateMachineInputs('State Machine 1');
-                const bumpTrigger = inputs.find(i => i.name === 'Trigger 2');
-                bumpTrigger.fire();
-                btn.onclick = (e) => {
-                    e.preventDefault();
-                    bumpTrigger.fire()
-                };
-            },
-            onStateChange: (event) => {
-                //btn.innerHTML = event.data[0];
-                
-                if(event.data[0] == 'Fireup' || event.data[0] == 'Flying') {
-                    btn.innerHTML = 'Uploading..'
-                    btn.style.backgroundColor = 'rgba(255,255,255,.5)'
-                    document.querySelector('body').style.backgroundColor = '#8cbdff'
-                }
+    if (bumpTrigger) {
+      bumpTrigger.fire();
 
-                else if(event.data[0] == 'Firedown') {
-                    btn.innerHTML = 'Finished!'
+      btn.onclick = (e) => {
+        e.preventDefault();
+        bumpTrigger.fire();
+      };
+    }
+  },
+  onStateChange: (event) => {
+    if (!event.data || !event.data[0]) return;
 
-                    setTimeout(() => {
-                        btn.style.backgroundColor = 'white'
-                        btn.innerHTML = 'Upload File'
-                        document.querySelector('body').style.backgroundColor = 'rgb(0, 110, 255)'
-                    }, 1000)
-                }
-            },
-        });
+    if (event.data[0] === 'Fireup' || event.data[0] === 'Flying') {
+      btn.innerHTML = 'Uploading..';
+      btn.style.backgroundColor = 'rgba(255,255,255,.5)';
+      document.body.style.backgroundColor = '#8cbdff';
+    } else if (event.data[0] === 'Firedown') {
+      btn.innerHTML = 'Finished!';
+
+      setTimeout(() => {
+        btn.style.backgroundColor = 'white';
+        btn.innerHTML = 'Upload File';
+        document.body.style.backgroundColor = 'rgb(0, 110, 255)';
+      }, 1000);
+    }
+  },
+});
