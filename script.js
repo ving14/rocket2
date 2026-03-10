cb5df5c9f131bdcbb7e0f7f0b92ae51f24285d5e
ving14 <ving14@stanford.edu>
2026-03-10 13:55:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
-* {
-  box-sizing: border-box;
-}
 
-body {
-  margin: 0;
-  min-height: 100vh;
-  display: grid;
-  place-items: center;
-  background: rgb(0, 110, 255);
-  font-family: Arial, sans-serif;
-}
+        const btn = document.querySelector('a')
+        const stateName = document.querySelector('p')
 
-.wrap {
-  position: relative;
-  width: min(90vw, 520px);
-  aspect-ratio: 1 / 1;
-}
+        const r = new rive.Rive({
+            src: 'https://assets.codepen.io/2621168/new_file_1.riv',
+            canvas: document.getElementById('canvas'),
+            autoplay: true,
+            stateMachines: 'State Machine 1',
+            fit: rive.Fit.cover,
+            onLoad: (_) => {
+                const inputs = r.stateMachineInputs('State Machine 1');
+                const bumpTrigger = inputs.find(i => i.name === 'Trigger 2');
+                bumpTrigger.fire();
+                btn.onclick = (e) => {
+                    e.preventDefault();
+                    bumpTrigger.fire()
+                };
+            },
+            onStateChange: (event) => {
+                //btn.innerHTML = event.data[0];
+                
+                if(event.data[0] == 'Fireup' || event.data[0] == 'Flying') {
+                    btn.innerHTML = 'Uploading..'
+                    btn.style.backgroundColor = 'rgba(255,255,255,.5)'
+                    document.querySelector('body').style.backgroundColor = '#8cbdff'
+                }
 
-canvas {
-  width: 100%;
-  height: 100%;
-  display: block;
-}
+                else if(event.data[0] == 'Firedown') {
+                    btn.innerHTML = 'Finished!'
 
-button {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  border: 0;
-  border-radius: 10px;
-  padding: 14px 24px;
-  background: white;
-  color: black;
-  font-size: 16px;
-  font-weight: 700;
-  cursor: pointer;
-}
-
-button:disabled {
-  cursor: default;
-}
+                    setTimeout(() => {
+                        btn.style.backgroundColor = 'white'
+                        btn.innerHTML = 'Upload File'
+                        document.querySelector('body').style.backgroundColor = 'rgb(0, 110, 255)'
+                    }, 1000)
+                }
+            },
+        });
